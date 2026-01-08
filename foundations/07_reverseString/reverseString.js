a3e1882cf3fc228b56c3b8907f163334042dd8f1
Paul van Niekerk <stormwindpro1@gmail.com>
2026-01-08 14:34:46 +0200

    forgot commit

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversedWord = "";
+    let character = "";
+    loopLength = word.length;
 
+
+    for(let i = loopLength; i > -1 ; i--){
+        character = word.charAt(i);
+        reversedWord += character;
+    }
+
+    return reversedWord;
 };
 
 // Do not edit below this line
