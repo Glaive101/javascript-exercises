4fbe0c7034ae7802f216411080fdfdb8d040192a
Paul van Niekerk <stormwindpro1@gmail.com>
2026-01-09 14:08:38 +0200

    temperature type conversion completed

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  converted = ((temp - 32) * 5 / 9);
+  if(converted%1 === 0){
+    return converted;
+  } else {
+    return parseFloat(converted.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  converted = (temp * 9 / 5 + 32);
+  if(converted%1 === 0){
+    return converted;
+  } else {
+    return parseFloat(converted.toFixed(1));
+  }
 };
 
 // Do not edit below this line
