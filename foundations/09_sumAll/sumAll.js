3402e01947dbdd88239f0713858fe628abc47ba6
Paul van Niekerk <stormwindpro1@gmail.com>
2026-01-09 13:46:57 +0200

    Sum and datatype checks completed

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
+    let start = 0; 
+    let end = 0;
 
+    if(first < 0 || second < 0) return "ERROR";
+    if(!Number.isInteger(first) || !Number.isInteger(second)) return "ERROR";
+
+    if(first < second){
+        start = first;
+        end = second;
+    } else {
+        start = second;
+        end = first;
+    }
+    console.log(`Start: ${start}, End: ${end}`);
+
+    for(let i = start; i <= end; i++){
+        //console.log(`Current sum value: ${sum}`);
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
